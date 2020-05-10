ef743f5592c271f7cddcfd87719cb99074a4c44e
Samantha Orizabal <sam.orizabal@gmail.com>
2020-05-10 05:18:15 +0000

    Hello, world

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,6 +55,9 @@ Rails.application.configure do
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
+  
+  # Allow connections to local server.
+  config.hosts.clear
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
