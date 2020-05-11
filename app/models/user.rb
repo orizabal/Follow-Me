823c1ae51cc66dd521030d2ded4452ce3f569c19
Samantha Orizabal <sam.orizabal@gmail.com>
2020-05-11 19:41:10 +0000

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+  
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   
   before_save { email.downcase! }
@@ -16,12 +18,35 @@ class User < ApplicationRecord
   has_secure_password
   validates :password,
     presence: true,
-    length: { minimum: 8 }
+    length: { minimum: 8 },
+    allow_nil: true # Allows nil passwords during user update. has_secure_password includes a separate validation that specifically catches nil passwords!
     
   # Returns the hash digest of the given string.
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+  
+  # Returns a random token.
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+  
+  # Remembers a user in the database for use in persistent sessions.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+  
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  
+  # Forgets a user.
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
