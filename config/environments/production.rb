089addd823557e59341a24dbfdc58fd2b5d731db
Samantha Orizabal <sam.orizabal@gmail.com>
2020-05-14 04:56:08 +0000

    Another test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,7 +101,6 @@ Rails.application.configure do
     :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true,
-    :ssl            => true,
     :tls            => true
   }
 
