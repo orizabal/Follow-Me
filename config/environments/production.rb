4c1092340a037b34bb444827bb687d04e96ac5c5
Samantha Orizabal <sam.orizabal@gmail.com>
2020-05-14 04:32:14 +0000

    Hopefully get SendGrid to work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,11 +91,11 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'hidden-citadel-99113.herokuapp.com'
+  host = 'https://hidden-citadel-99113.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
-    :port           => '587',
+    :port           => '465',
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
