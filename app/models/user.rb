5acfe98de5ae01504dc02874c9738ba4aa4725c9
Samantha Orizabal <sam.orizabal@gmail.com>
2020-05-14 05:57:38 +0000

    Try sending emails asynchronously

--- app/models/user.rb
+++ app/models/user.rb
@@ -61,7 +61,7 @@ class User < ApplicationRecord
 
   # Sends activation email.
   def send_activation_email
-    UserMailer.account_activation(self).deliver_now
+    UserMailer.delay.account_activation(self).deliver_now
   end
   
   private
